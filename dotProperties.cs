21fcc34f0fd55bb7f36cd6266db90ba940687897
nghess <nhess@uoregon.edu>
2024-04-06 21:36:17 -0700

    RDK v.1

---
+++ dotProperties.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotProperties : MonoBehaviour
+{
+    public bool isCoherent;
+    public Vector3 randomDirection;
+
+}
